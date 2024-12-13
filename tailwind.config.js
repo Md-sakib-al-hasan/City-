a66b9a27e62a1b654a5441a7002137a9d9295b70
Md Sakib al hasan <127658885+Md-sakib-al-hasan@users.noreply.github.com>
2024-12-13 22:14:37 +0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+import tailwindcssAnimated from 'tailwindcss-animated';
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {backgroundImage: {
-      'footer': "url('src/assets/Footer/Footer.png')",
-      'pantagon': "url('src/assets/Header/pantagon.jpg')",
+    extend: {
+      backgroundImage: {
+        'footer': "url('src/assets/Footer/Footer.png')",
+        'pantagon': "url('src/assets/Header/pantagon.jpg')",
+      },
+      content: {
+        "['']": "''",
+      },
     },
-    content: {
-      "['']": "''",
-    },
-  },
   },
   plugins: [
-    require('daisyui'),
-    require('tailwindcss-animated'),
+    daisyui,
+    tailwindcssAnimated,
   ],
-}
-
+};
